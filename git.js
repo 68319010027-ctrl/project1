b0bef15e69bd40037f11e4a3a5114060e378c59f
pat <68319010027@pic.ac.th>
2026-06-16 10:16:17 +0700

    Update git.js 2000

--- git.js
+++ git.js
@@ -2,3 +2,5 @@ document.write("Pimchanok")
 
 document.write("เอ๋เอ๋ ืเสื้อขาวเอ้าหน่อยยย")
 document.write("อยากนอนนนนนนนนนนน")
+document.write("เอ๋เอ๋ ืเสื้อขาวเอ้าหน่อยยย")
+document.write("อยากนอนนนนนนนนนนนจังโว้ยยยยยยยยยยย")
